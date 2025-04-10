970474e16694a7296e489955953d7fd87c47894b
Juan Revello <juan.revello@tiendanube.com>
2025-04-10 11:59:06 -0300

    delete unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,9 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Profile } from './users/profile.entity';
-import { PostsController } from './posts/posts.controller';
 import { PostsModule } from './posts/posts.module';
 import { Post } from './posts/post.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
